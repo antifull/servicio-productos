8b7d6a6792cf739d73d191c60d4d9957c796c2d0
Francisco Rubio Santiesteban <115087513+antifull@users.noreply.github.com>
2024-05-19 18:12:43 -0300

    Servicios de productos

---
+++ src/test/java/com/antifull/servicioproductos/ServicioProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antifull.servicioproductos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioProductosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
